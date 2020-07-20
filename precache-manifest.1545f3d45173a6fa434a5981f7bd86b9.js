2e170fcbac32a89bf1814c9e3fe8eba15f262473
lisawcoding <tolisacoding@gmail.com>
2020-07-20 19:57:56 +0800

    Updates

---
+++ precache-manifest.1545f3d45173a6fa434a5981f7bd86b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ca16f42882c08076510f92dd713fd4",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "3cad922d0137a66be6ca",
+    "url": "/TodoList/static/css/main.5d4e4aec.chunk.css"
+  },
+  {
+    "revision": "ac54b07372f25fbf27e0",
+    "url": "/TodoList/static/js/2.275325d9.chunk.js"
+  },
+  {
+    "revision": "2954b1568600ec1d7b70d079ce9ac1a4",
+    "url": "/TodoList/static/js/2.275325d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cad922d0137a66be6ca",
+    "url": "/TodoList/static/js/main.da78f589.chunk.js"
+  },
+  {
+    "revision": "0361186e564f6cc01b58",
+    "url": "/TodoList/static/js/runtime-main.9cc9eda5.js"
+  }
+]);
